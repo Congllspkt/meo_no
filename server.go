dc9b930e4c3b3d9595b5ad55d114658f9622f60e
DESKTOP-CSM9KCK\Welcome <cuong.nguyenkim@posco.net>
2024-09-10 08:00:29 +0700

    'm'

--- server.go
+++ server.go
@@ -6,38 +6,45 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	_ "github.com/lib/pq"
+	_ "github.com/go-sql-driver/mysql"
 )
 
-var connStr = "user=root password=345FSDF$# dbname=meono sslmode=disable"
-var db, _ = sql.Open("postgres", connStr)
+var dsn = "root:345FSDF$#@tcp(localhost:3306)/meono"
+var db, _ = sql.Open("mysql", dsn)
 
 func main() {
 	r := gin.Default()
 
-	r.GET("/getGame", getGame)
-	r.GET("/UpdateWaitStatus", UpdateWaitStatus)
-	r.GET("/UpdatePlayStatus", UpdatePlayStatus)
-	r.GET("/UpdatePlayer", UpdatePlayer)
+	r.GET("/register", register)
 
-	
+	r.GET("/getGame", getGame)
+	r.GET("/updateWaitStatus", updateWaitStatus)
+	r.GET("/updatePlayStatus", updatePlayStatus)
+	r.GET("/updatePlayer", updatePlayer)
+
 	r.GET("/getAllUser", getAllUser)
-	r.GET("/UpdateArr", UpdateArr)
+	r.GET("/updateArr", updateArr)
 
 	r.Run()
 }
 
-func UpdatePlayer(c *gin.Context) {
+func register(c *gin.Context) {
+	var minID int
+	db.QueryRow("SELECT MIN(idn) FROM user_tb WHERE username = ''").Scan(&minID)
+	db.Exec("UPDATE user_tb SET username = ? WHERE idn = ?", c.Query("name"), minID)
+}
+
+func updatePlayer(c *gin.Context) {
 	db.Exec("UPDATE game_tb SET playuser = $1", c.Query("p"))
 	getGame(c)
 }
 
-func UpdateWaitStatus(c *gin.Context) {
+func updateWaitStatus(c *gin.Context) {
 	db.Exec("UPDATE game_tb SET status = $1", utildata.WAITING)
 	getGame(c)
 }
 
-func UpdatePlayStatus(c *gin.Context) {
+func updatePlayStatus(c *gin.Context) {
 	db.Exec("UPDATE game_tb SET status = $1", utildata.PLAYING)
 	getGame(c)
 }
@@ -46,11 +53,11 @@ func getGame(c *gin.Context) {
 	rows, _ := db.Query("SELECT * FROM game_tb;")
 	var games []map[string]interface{}
 	for rows.Next() {
-		var id int
+		var idgame int
 		var status, playuser string
-		rows.Scan(&id, &status, &playuser)
+		rows.Scan(&idgame, &status, &playuser)
 		game := map[string]interface{}{
-			"id":       id,
+			"idgame":   idgame,
 			"status":   status,
 			"playuser": playuser,
 		}
@@ -60,26 +67,26 @@ func getGame(c *gin.Context) {
 
 	g := games[0]
 	c.JSON(http.StatusOK, gin.H{
-		"id":       g["id"],
+		"idgame":   g["idgame"],
 		"status":   g["status"],
 		"playuser": g["playuser"],
 	})
 }
 
-func UpdateArr(c *gin.Context) {
-	db.Exec("UPDATE user_tb SET arr = $1 where id = $2", c.Query("arr"), c.Query("id"))
+func updateArr(c *gin.Context) {
+	db.Exec("UPDATE user_tb SET arr = $1 where idn = $2", c.Query("arr"), c.Query("idn"))
 	getAllUser(c)
 }
 
 func getAllUser(c *gin.Context) {
-	rows, _ := db.Query("SELECT * FROM user_tb where play is not null;")
+	rows, _ := db.Query("SELECT idn, username, play  FROM user_tb where play is not null;")
 	var users []map[string]interface{}
 	for rows.Next() {
-		var id int
+		var idn int
 		var username, arr, play string
-		rows.Scan(&id, &username, &arr, &play)
+		rows.Scan(&idn, &username, &play, &arr)
 		user := map[string]interface{}{
-			"id":       id,
+			"idn":   idn,
 			"username": username,
 			"arr":      arr,
 			"play":     play,
